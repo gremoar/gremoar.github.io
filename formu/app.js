2675e3af191f0b844daf4798388efc00cfd3ad68
gremoar <116038475+gremoar@users.noreply.github.com>
2022-10-25 00:24:31 -0300

    Add files via upload

--- formu/app.js
+++ formu/app.js
@@ -1,71 +1,20 @@
-window.addEventListener('load', ()=> {
-    const form = document.querySelector('#formulario')
-    const usuario = document.getElementById('usuario')
-    const usuario_2 = document.getElementById('usuario_2')
-    const email = document.getElementById('email')
-    const tel = document.getElementById('tel')
-    const passConfirma = document.getElementById('passConfirma')
+const $form = document.querySelector('#formulario')
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        validaCampos()
+$form.addEventListener('submit',handleSubmit )
+
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'accept': 'application/json'
+        }
     })
-    
-    const validaCampos = ()=> {
-        //capturar los valores ingresados por el usuario
-        const usuarioValor = usuario.value.trim()
-        const usuario_2Valor = usuario_2.value.trim()
-        const emailValor = email.value.trim()
-        const telValor = tel.value.trim()
-     
-        //validando campo usuario
-        //(!usuarioValor) ? console.log('CAMPO VACIO') : console.log(usuarioValor)
-        if(!usuarioValor){
-            //console.log('CAMPO VACIO')
-            validaFalla(usuario, 'Campo vacío')
-        }else{
-            validaOk(usuario)
-        }
-
-        if(!usuario_2Valor){
-            //console.log('CAMPO VACIO')
-            validaFalla(usuario_2, 'Campo vacío')
-        }else{
-            validaOk(usuario_2)
-        }
-
-        //validando campo email
-        if(!emailValor){
-            validaFalla(email, 'Campo vacío')            
-        }else if(!validaEmail(emailValor)) {
-            validaFalla(email, 'El e-mail no es válido')
-        }else {
-            validaOk(email)
-        }
-         //validando campo password      
-         if(!telValor) {
-             validaFalla(tel, 'Campo vacío')
-         } else if (telValor.length < 10) {             
-             validaFalla(tel, 'Debe tener 10 caracteres cómo mínimo.')
-         } else {
-             validaOk(tel)
-         }
+    if(response.ok)
+    {
+        this.reset()
+        alert('gracias por contactarnos')
     }
-
-    const validaFalla = (input, msje) => {
-        const formControl = input.parentElement
-        const aviso = formControl.querySelector('p')
-        aviso.innerText = msje
-
-        formControl.className = 'form-control falla'
-    }
-    const validaOk = (input, msje) => {
-        const formControl = input.parentElement
-        formControl.className = 'form-control ok'
-    }
-
-    const validaEmail = (email) => {
-        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);        
-    }
-
-})
+}
